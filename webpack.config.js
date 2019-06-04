896bc4f1c465e91a90264ddece08c210dc8b7345
cnzmg <cnzmg@qq.com>
2019-06-04 18:04:23 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var [path, HtmlWebpackPlugin, MiniCssExtractPlugin, optimizecssassets, uglifyjs]
 ];
 
 module.exports = {
-    mode: 'production',  //模式 production development
+    mode: 'development',  //模式 production development
     optimization: {
         minimizer: [
             new uglifyjs({
@@ -13,9 +13,11 @@ module.exports = {
         ]
     },
     entry: {
-        configration: './src/public/javascripts/interactive/configuraction.js',
-        login: './src/public/javascripts/interactive/login.js',
-        index: './src/public/javascripts/interactive/index.js'
+        configration: './src/public/javascripts/interactive/configuraction.js',  //配置文件
+        login: './src/public/javascripts/interactive/login.js', //登陆操作
+        index: './src/public/javascripts/interactive/index.js', //common 首页
+        list: './src/public/javascripts/interactive/list.js', // 列表页
+        crud: './src/public/javascripts/interactive/crud.js'  //增删改查
     },
     devServer: {
         port: 3000,
@@ -32,16 +34,6 @@ module.exports = {
         new HtmlWebpackPlugin({  //login
             template: './src/views/login.html',
             filename: '../login.htm',
-            minify: {
-                removeAttributeQuotes: true,
-                collapseWhitespace: true
-            },
-            hash: true,
-            chunks: ['configration', 'login']
-        }),
-        new HtmlWebpackPlugin({  //tables
-            template: './src/views/tables.html',
-            filename: '../tables.htm',
             minify: {
                 removeAttributeQuotes: true,
                 collapseWhitespace: true
@@ -68,6 +60,236 @@ module.exports = {
             },
             hash: true,
             chunks: ['']
+        }),
+        new HtmlWebpackPlugin({  //formulaList
+            template: './src/views/formulaList.html',
+            filename: '../views/formulaList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //tables
+            template: './src/views/tables.html',
+            filename: '../views/tables.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  // u_Journal
+            template: './src/views/u_Journal.html',
+            filename: '../views/u_Journal.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //productList
+            template: './src/views/productList.html',
+            filename: '../views/productList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //detailedList
+            template: './src/views/detailedList.html',
+            filename: '../views/detailedList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //equipmentList
+            template: './src/views/equipmentList.html',
+            filename: '../views/equipmentList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //equipmentLongUpdate
+            template: './src/views/equipmentLongUpdate.html',
+            filename: '../views/equipmentLongUpdate.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //smallLocationConfig
+            template: './src/views/smallLocationConfig.html',
+            filename: '../views/smallLocationConfig.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //advertisementRootList adRootDetailedList
+            template: './src/views/advertisementRootList.html',
+            filename: '../views/advertisementRootList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  // adRootDetailedList
+            template: './src/views/adRootDetailedList.html',
+            filename: '../views/adRootDetailedList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  // chartsFinance
+            template: './src/views/chartsFinance.html',
+            filename: '../views/chartsFinance.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  // chartsActive systemUserList
+            template: './src/views/chartsActive.html',
+            filename: '../views/chartsActive.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //systemUserList 
+            template: './src/views/systemUserList.html',
+            filename: '../views/systemUserList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  // systemUserLvList
+            template: './src/views/systemUserLvList.html',
+            filename: '../views/systemUserLvList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  // feedbackList
+            template: './src/views/feedbackList.html',
+            filename: '../views/feedbackList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  // couponList 
+            template: './src/views/couponList.html',
+            filename: '../views/couponList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //  orderList 
+            template: './src/views/orderList.html',
+            filename: '../views/orderList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //  refundOrder 
+            template: './src/views/refundOrder.html',
+            filename: '../views/refundOrder.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //  orderEverDayList
+            template: './src/views/orderEverDayList.html',
+            filename: '../views/orderEverDayList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //  financialManagement
+            template: './src/views/financialManagement.html',
+            filename: '../views/financialManagement.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //  RepairPersonnelList 
+            template: './src/views/RepairPersonnelList.html',
+            filename: '../views/RepairPersonnelList.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //  materialLog  
+            template: './src/views/materialLog.html',
+            filename: '../views/materialLog.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'list']
+        }),
+        new HtmlWebpackPlugin({  //  编辑添加之类的操作  
+            template: './src/views/AddFormula.html',
+            filename: '../views/AddFormula.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true
+            },
+            hash: true,
+            chunks: ['configration', 'crud']
         }),
         new MiniCssExtractPlugin({
             template: './src/public/stylesheets/base/style.css',
